8830297d224b48ce4973d1cecd2f6dab9fa3eabe
MrOden911 <oden911@yandex.ru>
2017-11-12 12:09:39 +0300

    Вывод в файл и обработка ошибок

--- Sorting_lvl3.py
+++ Sorting_lvl3.py
@@ -1,13 +1,31 @@
-from pprint import pprint
+import sys
 
 input_file = open('input_data_lvl3.txt')
 work_list = input_file.read().splitlines()
 input_file.close()
 
-work_dict = {}
-for i in work_list:
-    work_dict.update({i: [int(i[-1]), False, '']})
 leng = len(work_list)
+
+output_file = open('output_file_lvl3.txt', 'w')
+
+false_list = []
+for i in range(leng):
+    for j in range(i+1, leng):
+        if work_list[i] == work_list[j]:
+            false_list.append([work_list[i], i, j])
+if false_list != []:
+    output_file.write('WARNING!\nОдинаковые позиции:\n')
+    for i in false_list:
+        output_file.write('{0} в строках {1} и {2}\n'.format(i[0], i[1], i[2]))
+    output_file.close()
+    sys.exit()
+
+work_dict = {}
+for i in range(leng):
+    type_numb = int(work_list[i][-1])
+    work_list[i] = work_list[i][0:-2]
+    work_dict.update({work_list[i]: [type_numb, False, '']})
+
 
 for i in range(len(work_list)):
     if work_dict[work_list[i]][0] == 2 or work_dict[work_list[i]][0] == 3 and work_dict[work_list[i]][1] is False:
@@ -26,9 +44,15 @@ for i in range(len(work_list)):
                 work_dict[work_list[i]][1] = True
                 j += 1
 
-false_number = 0
-for i in work_list:
-    if work_dict[i][1] is False:
-        false_number += 1
-print("Ошибок: ", false_number)
-pprint(work_dict)
+false_list = []
+for i in range(leng):
+    if work_dict[work_list[i]][1] is False:
+        false_list.append([work_list[i], i])
+if false_list != []:
+    output_file.write('WARNING!\nНе учтены позиции:\n')
+    for i in false_list:
+        output_file.write('{0} в строчке {1}\n'.format(i[0], i[1]))
+else:
+    for i in work_list:
+        output_file.write('{0} -> {1}\n'.format(i, work_dict[i][2]))
+output_file.close()
