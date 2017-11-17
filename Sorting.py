5b9922e42204d5493ca77fccc758246fd5bb3384
MrOden911 <oden911@yandex.ru>
2017-11-17 22:31:58 +0300

    Мелкие изменения

--- Sorting.py
+++ Sorting.py
@@ -1,3 +1,5 @@
+from Checker import check
+
 
 def sort():
     input_file = open('generated_data.txt')
@@ -8,22 +10,6 @@ def sort():
 
     false_list = []
     first_list = []
-
-    # for i in range(leng):
-    #     for j in range(i+1, leng):
-    #         if work_list[i] == work_list[j]:
-    #             false_list.append([work_list[i], i, j])
-    #             first_list.append(j)
-    # first_list = list(set(first_list))
-    # first_list.reverse()
-    # if false_list != []:
-    #     for i in first_list:
-    #         work_list.pop(i)
-    #     output_file.write('WARNING!\nОдинаковые позиции:\n')
-    #     for i in false_list:
-    #         output_file.write('{0} в строках {1} и {2}\n'.format(i[0][:-2], i[1]+1, i[2]+1))
-    #     output_file.write('Удалено строк: {}\n\n\n'.format(len(first_list)))
-
     li = []
     for i in work_list:
         if i not in li:
@@ -31,16 +17,15 @@ def sort():
         else:
             false_list.append([i, work_list.index(i), len(li)])
     if false_list != []:
-        output_file.write('WARNING!\nОдинаковые позиции:\n')
+        output_file.write('# WARNING!\n# Одинаковые позиции:\n')
         for i in false_list:
-            output_file.write('{0} в строках {1} и {2}\n'.format(i[0][:-2], i[1]+1, i[2]+1))
-        output_file.write('Удалено строк: {}\n\n\n'.format(len(false_list)))
+            output_file.write('# {0} в строках {1} и {2}\n'.format(i[0][:-2], i[1]+1, i[2]+1))
+        output_file.write('# Удалено строк: {}\n\n\n'.format(len(false_list)))
     work_list = li
     work_dict = {}
     leng = len(work_list)
     for i in range(leng):
         type_numb = int(work_list[i][-1])
-        work_list[i] = work_list[i][0:-2]
         work_dict.update({work_list[i]: [type_numb, False, '']})
 
     for i in range(len(work_list)):
@@ -50,15 +35,14 @@ def sort():
                 while j != 0 and work_dict[work_list[j-1]][1] is False and work_dict[work_list[i]][0] - work_dict[work_list[j-1]][0] == 1:
                     work_dict[work_list[j-1]][1] = True
                     work_dict[work_list[j-1]][2] = work_list[i]
-                    work_dict[work_list[i]][1] = True
                     j -= 1
-
+                work_dict[work_list[i]][1] = True
             elif i != leng-1 and j != leng-1 and work_dict[work_list[j+1]][1] is False and work_dict[work_list[i]][0] - work_dict[work_list[j+1]][0] == 1:
                 while j != leng-1 and work_dict[work_list[j+1]][1] is False and work_dict[work_list[i]][0] - work_dict[work_list[j+1]][0] == 1:
                     work_dict[work_list[j+1]][1] = True
                     work_dict[work_list[j+1]][2] = work_list[i]
-                    work_dict[work_list[i]][1] = True
                     j += 1
+                work_dict[work_list[i]][1] = True
     false_list = []
     for i in range(leng):
         if work_dict[work_list[i]][1] is False:
@@ -66,14 +50,14 @@ def sort():
             work_dict[work_list[i]][2] = 'ERROR'
 
     if false_list != []:
-        output_file.write('WARNING!\nНе учтены позиции:\n')
+        output_file.write('# WARNING!\n# Не учтены позиции:\n')
         for i in false_list:
-            output_file.write('{0} в строке {1}\n'.format(i[0], i[1]))
+            output_file.write('# {0} в строке {1}\n'.format(i[0], i[1]))
         output_file.write('\n\n')
 
     for i in work_list:
-        output_file.write('{0} -> {1}\n'.format(i, work_dict[i][2]))
+        if work_dict[i][2] != '':
+            output_file.write('{0} -> {1}\n'.format(i, work_dict[i][2]))
     output_file.close()
-
-
+    check()
 
